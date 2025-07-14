bc8a989b583ce9f0c7582dbd4afce910b497b467
Alexis Aguilar <98043211+alexisintech@users.noreply.github.com>
2025-07-14 14:41:13 -0400

    update

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <App />
     </ClerkProvider>
   </StrictMode>
