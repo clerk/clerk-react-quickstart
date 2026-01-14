15b3ea1e67cc335b6ee3913595b7030be4292b8a
Alexis Aguilar <98043211+alexisintech@users.noreply.github.com>
2026-01-14 16:28:51 -0700

    signedin/signedout --> show

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { ClerkProvider } from '@clerk/clerk-react';
+import { ClerkProvider } from '@clerk/react';
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
